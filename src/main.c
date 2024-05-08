63d8dca4d18317d3fbe7a7d5a5abd8053da141ec
Aven Bross <kieroda@gmail.com>
2024-05-08 17:39:20 -0500

    Exiting without crashing

--- src/main.c
+++ src/main.c
@@ -1,2 +1,27 @@
-void _cstart(int argc, char **argv) {}
+typedef short i16;
+typedef unsigned short u16;
+typedef int i32;
+typedef unsigned int u32;
+typedef long i64;
+typedef unsigned long u64;
+
+u64 syscall0(u64 scid);
+u64 syscall1(u64 scid, u64 a1);
+u64 syscall2(u64 scid, u64 a1, u64 a2);
+u64 syscall3(u64 scid, u64 a1, u64 a2, u64 a3);
+u64 syscall4(u64 scid, u64 a1, u64 a2, u64 a3, u64 a4);
+u64 syscall5(u64 scid, u64 a1, u64 a2, u64 a3, u64 a4, u64 a5);
+u64 syscall6(u64 scid, u64 a1, u64 a2, u64 a3, u64 a4, u64 a5, u64 a6);
+
+enum syscall {
+    SYS_EXIT = 60,
+};
+
+static void exit(i32 error_code) {
+    syscall1(SYS_EXIT, (u64)error_code);
+}
+
+void _cstart(i32 argc, char **argv) {
+    exit(0);
+}
 
--- src/runtime.s
+++ src/runtime.s
@@ -1,4 +1,82 @@
 .text
+.global syscall0
+syscall0:
+    movq %rdi, %rax
+    syscall
+    ret
+.type syscall0, @function
+.size syscall0, .-syscall0
+
+.global syscall1
+syscall1:
+    movq %rdi, %rax
+    movq %rsi, %rdi
+    syscall
+    ret
+.type syscall1, @function
+.size syscall1, .-syscall1
+
+.global syscall2
+syscall2:
+    movq %rdi, %rax
+    movq %rsi, %rdi
+    movq %rdx, %rsi
+    syscall
+    ret
+.type syscall2, @function
+.size syscall2, .-syscall2
+
+.global syscall3
+syscall3:
+    movq %rdi, %rax
+    movq %rsi, %rdi
+    movq %rdx, %rsi
+    movq %rcx, %rdx
+    syscall
+    ret
+.type syscall3, @function
+.size syscall3, .-syscall3
+
+.global syscall4
+syscall4:
+    movq %rdi, %rax
+    movq %rsi, %rdi
+    movq %rdx, %rsi
+    movq %rcx, %rdx
+    movq %r8, %r10
+    syscall
+    ret
+.type syscall4, @function
+.size syscall4, .-syscall4
+
+.global syscall5
+syscall5:
+    movq %rdi, %rax
+    movq %rsi, %rdi
+    movq %rdx, %rsi
+    movq %rcx, %rdx
+    movq %r8, %r10
+    movq %r9, %r8
+    syscall
+    ret
+.type syscall5, @function
+.size syscall5, .-syscall5
+
+.global syscall6
+syscall6:
+    movq %r9, %r11
+    movq %rdi, %rax
+    movq 8(%rsp), %r9
+    movq %rsi, %rdi
+    movq %rdx, %rsi
+    movq %rcx, %rdx
+    movq %r8, %r10
+    movq %r11, %r8
+    syscall
+    ret
+.type syscall6, @function
+.size syscall6, .-syscall6
+
 .extern _cstart
 .global _start
 _start:
